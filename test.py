e4797663e14bf770a7d11e0faca0181767aaf41f
Ryan Hoque <ryanhoque2@gmail.com>
2016-11-12 15:31:58 -0800

    removed test; ready for pull request

--- test.py
+++
@@ -1,8 +0,0 @@
-import init_imgs
-import sys
-
-# test function.
-# ideally this will move to parse.py, with init_imgs at the beginning and the lookup as a mapped command
-
-d = init_imgs.get()
-print('moving to slide number', d[sys.argv[1]], 'to find', sys.argv[1])
