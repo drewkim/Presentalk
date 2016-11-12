63f0e22ff6b97ca5137147ae9872f6e0c8c9dc40
Ryan Hoque <ryanhoque2@gmail.com>
2016-11-12 14:19:53 -0800

    implemented finding slide number from image description

--- find_img.py
+++ find_img.py
@@ -3,4 +3,18 @@ import sys
 import os
 
 target = sys.argv[1]
-os.system('')
+os.system('ls | grep slide | wc -l > tmp.txt')
+with open('tmp.txt') as fh:
+	length = int(fh.readlines()[0].rstrip().lstrip())
+for i in range(1, length+1):
+	os.system('ls slide'+str(i)+' > tmp.txt')
+	with open('tmp.txt') as fh:
+		for line in fh:
+			filepath = 'slide'+str(i)+'/'+line.rstrip()
+			os.system('./label.py '+filepath+' > tmp2.txt')
+			with open('tmp2.txt') as fh2:
+				if fh2.readlines()[0].split()[2] == target:
+					print('*moving to slide number '+str(i)+'*')
+
+os.system('rm tmp.txt')
+os.system('rm tmp2.txt')
