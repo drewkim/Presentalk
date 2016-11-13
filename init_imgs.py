b37760948df992570532f50a78e11d5488ab9016
Ryan Hoque <ryanhoque2@gmail.com>
2016-11-13 03:17:29 -0800

    slightly modified init_imgs

--- init_imgs.py
+++ init_imgs.py
@@ -4,14 +4,14 @@ import os
 def get():
 	os.system('export GOOGLE_APPLICATION_CREDENTIALS=\'Presentalk-e0eb7b7c46fb.json\'')
 	d = dict()
-	os.system('ls | grep slide | wc -l > tmp.txt')
+	os.system('ls parser | grep slide | wc -l > tmp.txt')
 	with open('tmp.txt') as fh:
 		length = int(fh.readlines()[0].rstrip().lstrip())
 	for i in range(1, length+1):
-		os.system('ls slide'+str(i)+' > tmp.txt')
+		os.system('ls parser/slide'+str(i)+' > tmp.txt')
 		with open('tmp.txt') as fh:
 			for line in fh:
-				filepath = 'slide'+str(i)+'/'+line.rstrip()
+				filepath = 'parser/slide'+str(i)+'/'+line.rstrip()
 				os.system('./label.py '+filepath+' > tmp2.txt')
 				with open('tmp2.txt') as fh2:
 					d[fh2.readlines()[0].split()[2]] = i
