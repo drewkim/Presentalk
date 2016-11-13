27cc471cfb21942bf44faea4f151452efbfefc37
kkranen <kyle@Kyles-Air.local>
2016-11-12 20:32:40 -0800

    Added a retrieve for google images based on keyword.

---
+++ GoogleCSEGet.py
@@ -0,0 +1,12 @@
+import json
+import urllib.request
+def get(arg):
+	response = urllib.request.urlopen("https://www.googleapis.com/customsearch/v1?key=AIzaSyDAmhrmizdXy8fCA1XOhRREoqx0E6wu-s4&cx=016734205687455053515:rtx4ejkkseu&defaultToImageSearch=True&searchtype=image&q=" + arg)
+	html = response.read().decode('utf-8')
+	html_split = html.split()
+	print(html_split)
+	for i in range(0,len(html_split)):
+		if html_split[i] == "\"cse_image\":":
+			return html_split[i + 4].replace("\"", "")
+
+	
